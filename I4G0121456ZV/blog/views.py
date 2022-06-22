af8c7108e62f083c068eb8a59b548bf624d64cc2
Musoye <oyebamijimustapha44@gmail.com>
2022-06-22 12:56:02 -1100

    the full page design with django

---
+++ I4G0121456ZV/blog/views.py
@@ -0,0 +1,37 @@
+from audioop import reverse
+from django.shortcuts import render
+from .models import Post
+from django.urls import reverse_lazy
+
+# Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+
+class PostListView(ListView):
+
+    model = Post
+
+class PostCreateView(CreateView):
+
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+
+    model = Post
+
+class PostUpdateView(UpdateView):
+
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
